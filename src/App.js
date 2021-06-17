ae4bb8b602b46bb2a7df2bff219c1c38301d5e02
CLAIRE-LHV <claire.lhv@gmail.com>
2021-06-17 22:01:30 +1000

    creat-react-app changed to a simple form

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export function App() {
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.elements.guessNumber.value);
+  };
+  return (
+    <div className="App">
+      <h1>Guess the number</h1>
+      <form onSubmit={onSubmit}>
+        <label>Guessing number: </label>
+        <input name="guessNumber" type="number" min="1" max="100" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
